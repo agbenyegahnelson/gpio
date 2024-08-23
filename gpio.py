b480b641f771731c33aa5ae0f12946c46e14383d
nelson joel amuzu <nelsonjoelamuzu@gmail.com>
2024-08-23 16:35:58 +0000

    gpio and led

---
+++ gpio.py
@@ -0,0 +1,12 @@
+from gpiozero import LED
+from time import sleep
+led = LED(17)
+number = int(input('Enter a number'))
+
+while number > 0:
+    print('led running...' + str(number))
+    led.on()
+    sleep(1)
+    led.off()
+    sleep(1)
+    number-=1
